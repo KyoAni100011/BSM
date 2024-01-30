9c37b6efab700d8ef0a170dd0cbd7799da26a78b
Truong Hoang Kha <kha@gmail.com>
2024-01-30 23:54:51 +0700

    first commit

---
+++ src/main/java/App.java
@@ -0,0 +1,26 @@
+package main.java;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/example.fxml"));
+
+        Scene scene = new Scene(root);
+
+        stage.setTitle("FXML Welcome");
+        stage.setWidth(root.prefWidth(-1));
+        stage.setHeight(root.prefHeight(-1));
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
