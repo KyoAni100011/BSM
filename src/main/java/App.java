f5b2bdd777a7e70492c3566469a5cd3a0c0c3ea5
Aplus59 <94693152+Aplus59@users.noreply.github.com>
2024-02-21 21:19:20 +0700

    done check field in log in
    code front check field in log in

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,14 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.scene.input.MouseEvent;
+
 public class App extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../resources/login.fxml"));
 
         Scene scene = new Scene(root);
 
-        stage.setTitle("FXML Welcome");
+        root.setOnMousePressed((MouseEvent event) -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        root.setOnMouseDragged((MouseEvent event) -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.TRANSPARENT);
         stage.setWidth(root.prefWidth(-1));
         stage.setHeight(root.prefHeight(-1));
         stage.setScene(scene);
