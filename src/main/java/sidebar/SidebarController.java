2e3a30abdb2a6937cf24f8a84cc66527dc8c43ee
Truong Hoang Kha <kha@gmail.com>
2024-02-24 19:42:05 +0700

    Add sidebar naviagtion

--- src/main/java/sidebar/SidebarController.java
+++ src/main/java/sidebar/SidebarController.java
@@ -1,7 +1,11 @@
 package main.java.sidebar;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
@@ -20,31 +24,39 @@ public class SidebarController {
     private Button revenueByCustomer;
 
     @FXML
+    private Button revenueByEmployee;
+
+    @FXML
     private Button userAccount;
 
     @FXML
-    void SwitchBookRevenue(ActionEvent event) {
-
+    void SwitchBookRevenue(ActionEvent event) throws IOException {
+        loadPage("bookRevenue");
     }
 
     @FXML
-    void SwitchCategoryRevenue(ActionEvent event) {
-
+    void SwitchCategoryRevenue(ActionEvent event) throws IOException {
+        loadPage("revenueByCustomer");
     }
 
     @FXML
-    void SwitchRevenueByCustomer(ActionEvent event) {
-
+    void SwitchRevenueByCustomer(ActionEvent event) throws IOException {
+        loadPage("revenueByCustomer");
     }
 
     @FXML
-    void SwitchRevenueByEmployee(ActionEvent event) {
-
+    void SwitchRevenueByEmployee(ActionEvent event) throws IOException {
+        loadPage("revenueByEmployee");
     }
 
     @FXML
-    void SwitchUserAccount(ActionEvent event) {
+    void SwitchUserAccount(ActionEvent event) throws IOException {
+        loadPage("userAccount");
+    }
 
+    private void loadPage(String page) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("../../resources/view/" + page + ".fxml"));
+        bp.setCenter(root);
     }
 
 }
