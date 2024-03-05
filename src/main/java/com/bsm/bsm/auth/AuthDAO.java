9b46adeb09f008bb8064f61c84b5c338c2e84887
Truong Hoang Kha <kha@gmail.com>
2024-03-05 08:26:15 +0700

    Change AuthDAO

--- src/main/java/com/bsm/bsm/auth/AuthDAO.java
+++ src/main/java/com/bsm/bsm/auth/AuthDAO.java
@@ -1,15 +1,14 @@
 package com.bsm.bsm.auth;
 
+import com.bsm.bsm.database.DatabaseConnection;
+import com.bsm.bsm.user.UserModel;
+import org.mindrot.jbcrypt.BCrypt;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Arrays;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
-
-import com.bsm.bsm.database.DatabaseConnection;
-import com.bsm.bsm.user.UserModel;
-import org.mindrot.jbcrypt.BCrypt;
 
 public class AuthDAO {
     private static final String SELECT_USER_QUERY = "SELECT * FROM user WHERE email = ?";
@@ -17,7 +16,7 @@ public class AuthDAO {
     public static boolean validateUser(String email, String password) {
         String QUERY_PASSWORD = "SELECT PASSWORD FROM user WHERE email='%s'".formatted(email);
 
-        try (ResultSet resultSet_ = DatabaseConnection.getConnection().createStatement().executeQuery(QUERY_PASSWORD)){
+        try (ResultSet resultSet_ = DatabaseConnection.getConnection().createStatement().executeQuery(QUERY_PASSWORD)) {
             if (resultSet_.next()) {
                 String storedPass = resultSet_.getString("password").trim();
 
@@ -28,7 +27,7 @@ public class AuthDAO {
                          ResultSet resultSet = secondStatement.executeQuery(QUERY_EMAIL)) {
 
                         if (resultSet.next()) {
-                            if (email.equals(resultSet.getString("email"))){
+                            if (email.equals(resultSet.getString("email"))) {
                                 return true;
                             }
                         } else {
@@ -53,21 +52,17 @@ public class AuthDAO {
     public static UserModel getUserInfo(String email, String password) {
         AtomicReference<UserModel> userModelRef = new AtomicReference<>();
 
-        if (validateUser(email, password)) {
-            DatabaseConnection.executeQuery(SELECT_USER_QUERY, resultSet -> {
-                if (resultSet.next()) {
-                    String id = Arrays.toString(resultSet.getBytes("id"));
-                    String dob = resultSet.getString("dob");
-                    String name = resultSet.getString("name");
-                    boolean isEnabled = resultSet.getBoolean("isEnabled");
-                    String password_ = resultSet.getString("password");
+        DatabaseConnection.executeQuery(SELECT_USER_QUERY, resultSet -> {
+            if (resultSet.next()) {
+                String id = Arrays.toString(resultSet.getBytes("id"));
+                String dob = resultSet.getString("dob");
+                String name = resultSet.getString("name");
+                boolean isEnabled = resultSet.getBoolean("isEnabled");
+                String password_ = resultSet.getString("password");
 
-                    userModelRef.set(new UserModel(id, name, email, password_, dob, isEnabled));
-                }
-            }, email);
-        } else {
-            userModelRef.set(null);
-        }
+                userModelRef.set(new UserModel(id, name, email, password_, dob, isEnabled));
+            }
+        }, email);
 
         return userModelRef.get();
     }
