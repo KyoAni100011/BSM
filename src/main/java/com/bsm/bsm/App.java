49baee81e7c4205ef9a03bbad4ecddc01ab2b773
Aplus59 <94693152+Aplus59@users.noreply.github.com>
2024-03-06 14:18:06 +0700

    fix login screen

--- src/main/java/com/bsm/bsm/App.java
+++ src/main/java/com/bsm/bsm/App.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 import java.util.Objects;
@@ -12,7 +13,17 @@ import java.util.Objects;
 public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/bsm/bsm/view/admin/adminMainScreen.fxml")));
+//        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/com/bsm/bsm/view/admin/adminMainScreen.fxml")));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/bsm/bsm/view/login.fxml"));
+        Parent root = loader.load();
+
+        // Check if the loaded FXML file is the login screen
+        if (loader.getLocation().getPath().contains("login.fxml")) {
+            stage.initStyle(StageStyle.UNDECORATED); // Hide the title bar for the login screen
+        } else {
+            stage.setTitle("FXML Welcome");
+        }
+
         Scene scene = new Scene(root);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("/com/bsm/bsm/style/style.css")).toExternalForm());
         stage.setTitle("FXML Welcome");
