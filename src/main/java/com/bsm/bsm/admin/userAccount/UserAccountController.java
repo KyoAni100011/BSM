630f7afc01246413b3a8850a82e489402e079189
TrietMinh23 <npmtriet2k3@gmail.com>
2024-03-11 22:00:04 +0700

    Update pagination buttons in userAccount.fxml

--- src/main/java/com/bsm/bsm/admin/userAccount/UserAccountController.java
+++ src/main/java/com/bsm/bsm/admin/userAccount/UserAccountController.java
@@ -3,6 +3,7 @@ package com.bsm.bsm.admin.userAccount;
 import com.bsm.bsm.admin.AdminModel;
 import com.bsm.bsm.user.UserModel;
 import com.bsm.bsm.user.UserSingleton;
+import com.bsm.bsm.utils.FXMLLoaderHelper;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -17,23 +18,44 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import java.util.ResourceBundle;
 
-public class UserAccountController{
-    public Button employeeButton, adminButton, addUserButton, passwordResetButton;
+public class UserAccountController implements Initializable {
+    @FXML
+    public Button employeeButton, adminButton;
+    @FXML
+    public Button addUserButton, passwordResetButton;
+    @FXML
+    public Button previousPaginationButton, nextPaginationButton, firstPaginationButton, secondPaginationButton, thirdPaginationButton, fourthPaginationButton, fifthPaginationButton;
     @FXML
     private VBox pnItems = null;
 
     private final UserAccountService userAccountService = new UserAccountService();
     private final UserModel adminInfo = UserSingleton.getInstance().getUser();
 
-    @FXML
-    private void initialize() throws IOException {
+    private int currentPage = 1;
+    private final int itemsPerPage = 9;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         employeeButton.getStyleClass().add("profile-setting-button");
         adminButton.getStyleClass().add("profile-setting-button");
         // Load all users initially
-        updateUsersList(".employee@bms.com");
-        updateButtonStyle(employeeButton);
+        try {
+            updateUsersList(".employee@bms.com");
+            updateButtonStyle(employeeButton);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Attach event handlers to pagination buttons
+        firstPaginationButton.setOnAction(this::handlePaginationButton);
+        secondPaginationButton.setOnAction(this::handlePaginationButton);
+        thirdPaginationButton.setOnAction(this::handlePaginationButton);
+        fourthPaginationButton.setOnAction(this::handlePaginationButton);
+        fifthPaginationButton.setOnAction(this::handlePaginationButton);
     }
 
     @FXML
@@ -50,12 +72,27 @@ public class UserAccountController{
 
     @FXML
     private void handlePasswordResetButton(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/bsm/bsm/view/admin/userAccount/passwordReset.fxml"));
-        Parent root = fxmlLoader.load();
-        Stage stage = new Stage();
-        stage.initModality(Modality.APPLICATION_MODAL);
-        stage.setScene(new Scene(root));
-        stage.showAndWait();
+        FXMLLoaderHelper.loadFXML(new Stage(), "admin/userAccount/passwordReset");
+    }
+
+    @FXML
+    private void handlePaginationButton(ActionEvent event) {
+        Button buttonClicked = (Button) event.getSource();
+        currentPage = Integer.parseInt(buttonClicked.getText());
+        // Call updateUsersList with the appropriate email suffix
+        if (employeeButton.getStyleClass().contains("profile-setting-button-admin")) {
+            try {
+                updateUsersList(".employee@bms.com");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                updateUsersList(".admin@bms.com");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private void updateButtonStyle(Button activeButton) {
@@ -70,25 +107,70 @@ public class UserAccountController{
         }
     }
 
-
-    private void updateUsersList(String emailSuffix) {
-        // Clear current items in the list
+    private void updateUsersList(String emailSuffix) throws IOException {
         pnItems.getChildren().clear();
 
         AdminModel adminModel = userAccountService.getAllUsersInfo(adminInfo.getId());
-        for (UserModel user : adminModel.viewUsers()) {
-            if (user.getEmail().endsWith(emailSuffix) || emailSuffix.isEmpty()) {
-                try {
-                    FXMLLoader fxmlLoader = new FXMLLoader();
-                    fxmlLoader.setLocation(getClass().getResource("/com/bsm/bsm/view/admin/userAccount/tableItem.fxml"));
-                    Node item = fxmlLoader.load();
-                    TableItemController tableItemController = fxmlLoader.getController();
-                    tableItemController.setUserModel(user);
-                    pnItems.getChildren().add(item);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+        List<UserModel> users = adminModel.viewUsers();
+        int startIndex = (currentPage - 1) * itemsPerPage;
+        int endIndex = Math.min(startIndex + itemsPerPage, users.size());
+
+        for (int i = startIndex; i < endIndex; i++) {
+            UserModel user = users.get(i);
+            if (user.getEmail().endsWith(emailSuffix)) {
+                FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/bsm/bsm/view/admin/userAccount/tableItem.fxml"));
+                Node item = fxmlLoader.load();
+                TableItemController tableItemController = fxmlLoader.getController();
+                tableItemController.setUserModel(user);
+                pnItems.getChildren().add(item);
             }
+        }
+
+        int totalPages = (int) Math.ceil((double) users.size() / itemsPerPage);
+        updatePaginationButtons(totalPages);
+    }
+
+    private void updatePaginationButtons(int totalPages) {
+        // Clear all pagination buttons visibility
+        for (Button button : Arrays.asList(firstPaginationButton, secondPaginationButton, thirdPaginationButton, fourthPaginationButton, fifthPaginationButton)) {
+            button.setVisible(false);
+        }
+
+        // Show pagination buttons based on the current page and total pages
+        if (totalPages > 1) {
+            int startPage = Math.max(1, Math.min(currentPage - 2, totalPages - 4));
+            int endPage = Math.min(startPage + 4, totalPages);
+
+            previousPaginationButton.setVisible(currentPage > 1);
+            nextPaginationButton.setVisible(currentPage < totalPages);
+
+            for (int i = startPage; i <= endPage; i++) {
+                Button button;
+                switch (i - startPage) {
+                    case 0:
+                        button = firstPaginationButton;
+                        break;
+                    case 1:
+                        button = secondPaginationButton;
+                        break;
+                    case 2:
+                        button = thirdPaginationButton;
+                        break;
+                    case 3:
+                        button = fourthPaginationButton;
+                        break;
+                    case 4:
+                        button = fifthPaginationButton;
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + (i - startPage));
+                }
+                button.setText(String.valueOf(i));
+                button.setVisible(true);
+            }
+        } else {
+            previousPaginationButton.setVisible(false);
+            nextPaginationButton.setVisible(false);
         }
     }
 }
