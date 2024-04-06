d9eec6902916ccf6ef234f8cbca7d9e8752faf0c
TrietMinh23 <npmtriet2k3@gmail.com>
2024-04-06 16:04:39 +0700

    [FE] [Employee] Add a new book.

--- src/main/java/com/bsm/bsm/employee/sidebarController.java
+++ src/main/java/com/bsm/bsm/employee/sidebarController.java
@@ -82,7 +82,7 @@ public class sidebarController {
 
     @FXML
     void SwitchBook(ActionEvent event) throws IOException {
-        loadPage("book/updateBook");
+        loadPage("book/addBook");
     }
 
     @FXML
