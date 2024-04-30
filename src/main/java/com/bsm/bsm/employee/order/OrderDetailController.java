c1e7da0b4e69648f661b71550e73c6819d3942f8
Aplus59 <94693152+Aplus59@users.noreply.github.com>
2024-05-01 00:32:03 +0700

    fix bug date importsheet, order detail

--- src/main/java/com/bsm/bsm/employee/order/OrderDetailController.java
+++ src/main/java/com/bsm/bsm/employee/order/OrderDetailController.java
@@ -3,19 +3,22 @@ package com.bsm.bsm.employee.order;
 import com.bsm.bsm.order.Order;
 import com.bsm.bsm.order.OrderBooksDetails;
 import com.bsm.bsm.order.OrderService;
+import com.bsm.bsm.utils.NumericValidationUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.VBox;
+import javafx.util.StringConverter;
 
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class OrderDetailController {
     private static final OrderService importSheetService = new OrderService();
@@ -23,7 +26,7 @@ public class OrderDetailController {
     public static List<OrderBooksDetails> listBook = new ArrayList<>();
     private static int id = 1;
     private static Order order;
-    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyy");
+    private final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     @FXML
     public TextField employeeNameField, totalPricefield, idField, customerNameField;
     @FXML
@@ -43,15 +46,33 @@ public class OrderDetailController {
         importDatePicker.getEditor().setOpacity(1);
         setOrderInfo();
         updateSheet();
-    }
+        setupDatePicker();
 
+    }
+    private void setupDatePicker() {
+        importDatePicker.setPromptText("dd/mm/yyyy");
+
+        importDatePicker.setConverter(new StringConverter<LocalDate>() {
+            @Override
+            public String toString(LocalDate date) {
+                return date != null ? dateFormatter.format(date) : "";
+            }
+
+            @Override
+            public LocalDate fromString(String string) {
+                return string != null && !string.isEmpty() ? LocalDate.parse(string, dateFormatter) : null;
+            }
+        });
+
+        importDatePicker.getEditor().addEventFilter(KeyEvent.KEY_TYPED, NumericValidationUtils.numericValidation(10));
+    }
     private void setOrderInfo() {
         idField.setText(String.valueOf(order.getId()));
         employeeNameField.setText(order.getEmployee().getName());
         customerNameField.setText(order.getCustomer().getName());
         totalPricefield.setText(String.valueOf(order.getTotalPrice()));
+        System.out.println("date" + order.getOrderDate() );
 
-        // pass data from table to detail and create to detail have dif day format
         importDatePicker.setValue(LocalDate.parse(order.getOrderDate(), dateFormatter));
     }
 
